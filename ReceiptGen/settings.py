4f533e34d4d106eda22658a1b6ac21773d35c129
veecee-tech <jethrovictor5@yahoo.com>
2023-03-08 20:42:17 +0100

    update

--- ReceiptGen/settings.py
+++ ReceiptGen/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # ROOT_PATH = os.path.dirname(__file__)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
-STATICFILES_DIRS =[os.path.join(BASE_DIR, "static"),]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# # STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
+# STATICFILES_DIRS =[os.path.join(BASE_DIR, "static"),]
 
 # CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 
